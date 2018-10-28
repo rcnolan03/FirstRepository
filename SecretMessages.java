366dd96e1be4ab1748717761f420835ddf35e9f3
rcnolan03 <Tre@DESKTOP-IIELU3B.local.tld>
2018-10-27 21:22:35 -0400

    Better file layout

--- SecretMessages.java
+++
@@ -1,8 +0,0 @@
-public class SecretMessages {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Tre
+ *
+ */
+module gg {
+}
